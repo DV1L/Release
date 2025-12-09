090aa8e9e305306d69e0a152c3fb3f45d827a4fa
DV1L <hdavidl0321@gmail.com>
2025-12-09 01:36:58 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="release",                                 # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="DV1L",                         # Tu nombre
+    author_email="hdavidl0321@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/dv1l/release",     # URL del proyecto
+)
